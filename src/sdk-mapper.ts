7004b4fdd619b392195e3fc68dfe23166c4409f0
richardgibson <rickityg@gmail.com>
2021-05-25 16:19:11 +0100

    kl-29 typed query parameters

--- src/sdk-mapper.ts
+++ src/sdk-mapper.ts
@@ -1,6 +1,6 @@
-import {JSONSchema} from "json-schema-to-typescript";
-import {OASParameter, OASPath, OASRef, OASRequestBody, OASResponse} from "../src/oas";
-import {OAS, OASOperation} from "./oas";
+import { JSONSchema } from "json-schema-to-typescript";
+import { OASParameter, OASPath, OASRef, OASRequestBody, OASResponse } from "../src/oas";
+import { OAS, OASOperation } from "./oas";
 
 function capitilize(name: string): string {
   return name.substring(0, 1).toUpperCase() + name.substring(1);
@@ -13,10 +13,10 @@ function pathName(path: string) {
   return path.split('/').map(name).join('');
 }
 
-function traverse<T>(parts: string[], obj: any): T | undefined{
-  if(obj && parts.length > 0 && typeof(obj) === 'object') {
+function traverse<T>(parts: string[], obj: any): T | undefined {
+  if (obj && parts.length > 0 && typeof (obj) === 'object') {
     const nextKey = parts[0];
-    if(Object.prototype.hasOwnProperty.call(obj, nextKey)) {
+    if (Object.prototype.hasOwnProperty.call(obj, nextKey)) {
       const next = obj[nextKey];
       if (parts.length === 1) return next;
       else return traverse<T>(parts.slice(1), next);
@@ -29,36 +29,46 @@ function traversePath<T>(path: string, obj: any): T {
   return traverse<T>(path.replace('#/', '').split('/'), obj) as T;
 }
 
-function pathsFrom(oas: OAS): Array<{path: string, definition: OASPath | undefined}> {
+function pathsFrom(oas: OAS): Array<{ path: string, definition: OASPath | undefined; }> {
   return Object.keys(oas.paths).map(key => {
     const value = oas.paths[key];
-    if(Object.prototype.hasOwnProperty.call(value, '$ref')) return { path: key, definition: traversePath((value as OASRef)['$ref'], oas)};
-    return { path: key, definition: value as OASPath}
-  })
+    if (Object.prototype.hasOwnProperty.call(value, '$ref')) return { path: key, definition: traversePath((value as OASRef)['$ref'], oas) };
+    return { path: key, definition: value as OASPath };
+  });
 }
-function methodOperations(path: OASPath): Array<{method: string; definition: OASOperation}> {
+function methodOperations(path: OASPath): Array<{ method: string; definition: OASOperation; }> {
   const keys = ['get', 'put', 'post', 'delete', 'options', 'head', 'patch', 'trace'];
   return Object.keys(path).filter(it => keys.includes(it)).map(key => {
     const operation = (path as any)[key] as OASOperation;
-    const definition: OASOperation = {...operation, parameters: [...(operation.parameters ?? []), ...(path.parameters ?? [])] }
-    return {method: key, definition};
+    const definition: OASOperation = { ...operation, parameters: [...(operation.parameters ?? []), ...(path.parameters ?? [])] };
+    return { method: key, definition };
   });
 }
 
 function operationParameters(oas: OAS, operation: OASOperation): OASParameter[] {
   return (operation.parameters ?? []).map(it => {
-    if(Object.prototype.hasOwnProperty.call(it, '$ref')) return traversePath((it as OASRef)['$ref'], oas);
+    if (Object.prototype.hasOwnProperty.call(it, '$ref')) return traversePath((it as OASRef)['$ref'], oas);
     return it as OASParameter;
   });
 }
 
 function pathParamsParam(pathParameters: string[]): string | undefined {
-  if(pathParameters.length === 0) return undefined;
+  if (pathParameters.length === 0) return undefined;
   return `params: {${pathParameters.map(it => `${it}: string`).join('; ')}}`;
 }
 
+function queryParamsParam(queryParamsParam: string[]): string | undefined {
+  if (queryParamsParam.length === 0) return undefined;
+  return `queryParameters: {${queryParamsParam.map((it) => it + '?: string').join('; ')}}`;
+}
+
+function multiQueryParamsParam(multiQueryParamsParam: string[]): string | undefined {
+  if (multiQueryParamsParam.length === 0) return undefined;
+  return `multiQueryParameters: {${multiQueryParamsParam.map((it) => it + '?: string[]').join('; ')}}`;
+}
+
 function headerParamsParam(headerParameters: string[]): string | undefined {
-  if(headerParameters.length === 0) return 'headers: Record<string, string>';
+  if (headerParameters.length === 0) return 'headers: Record<string, string>';
   return `headers: Record<string, string> & Partial<{${headerParameters.map(it => `'${it}': string`).join('; ')}}>`;
 }
 
@@ -66,12 +76,12 @@ function typeFrom(oas: OAS, response: OASResponse | OASRef): [string, string[]] 
   const def = Object.prototype.hasOwnProperty.call(response, '$ref') ? traversePath<OASResponse>((response as OASRef)['$ref'], oas) : response as OASResponse;
   const type = Object.keys(def.content ?? ['application/text'])[0];
   const ref = (def.content as any)[type].schema as OASRef;
-  if(type === 'application/json') {
+  if (type === 'application/json') {
     const refValue = ref['$ref'];
-    if(refValue) {
+    if (refValue) {
       const typeDef = traversePath<JSONSchema>(refValue, oas);
       const typeName = typeDef.title ?? refValue.substring(refValue.lastIndexOf('/') + 1);
-      return [typeName, [typeName]]
+      return [typeName, [typeName]];
     }
     return ['unknown', []];
   }
@@ -81,7 +91,7 @@ function typeFrom(oas: OAS, response: OASResponse | OASRef): [string, string[]] 
 function returnType(oas: OAS, method: OASOperation, imports: string[]): string {
   return Object.keys(method.responses).map(statusCode => {
     const response = method.responses[statusCode];
-    const [type, newImports] = typeFrom(oas, response)
+    const [type, newImports] = typeFrom(oas, response);
     imports.push(...newImports);
     return `{ statusCode: ${statusCode}; result: ${type} }`;
   }).join(' | ');
@@ -90,7 +100,7 @@ function returnType(oas: OAS, method: OASOperation, imports: string[]): string {
 function ifCode(code: string, json: boolean): string {
   return `if(result.statusCode === ${code}) {
         return { statusCode: ${code}, headers: result.headers, result: ${json ? `JSON.parse(result.body)` : 'result.body'}  };
-      }`
+      }`;
 }
 
 function bodyValue(oas: OAS, method: OASOperation): string {
@@ -104,13 +114,13 @@ function bodyValue(oas: OAS, method: OASOperation): string {
 }
 
 function bodyFrom(oas: OAS, operation: OASOperation, imports: string[]): string | undefined {
-  if(!operation.requestBody) return undefined;
+  if (!operation.requestBody) return undefined;
   const bodyDefinition = Object.prototype.hasOwnProperty.call(operation.requestBody, '$ref') ? traversePath<OASRequestBody>((operation.requestBody as OASRef)['$ref'], oas) : operation.requestBody as OASRequestBody;
   const type = Object.keys(bodyDefinition.content ?? ['application/text'])[0];
   const ref = (bodyDefinition.content as any)[type].schema as OASRef;
-  if(type === 'application/json') {
+  if (type === 'application/json') {
     const refValue = ref['$ref'];
-    if(refValue){
+    if (refValue) {
       const imp = refValue.substring(refValue.lastIndexOf('/') + 1);
       imports.push(imp);
       return imp;
@@ -124,13 +134,16 @@ function sdkMethod(path: string, method: string, pathParameters: string[], query
   const pathParams = pathParamsParam(pathParameters);
   const headerParams = headerParamsParam(headerParameters);
   const bodyParam = bodyFrom(oas, methodDefinition, imports);
-  const params = [pathParams, bodyParam && `body: ${bodyParam}`, 'queryParameters: Record<string, string> = {}', headerParams + ' = {}'].filter(it => !!it);
+  const queryParams = queryParamsParam(queryParameters);
+  const multiQueryParams = multiQueryParamsParam(queryParameters);
+  const params = [pathParams, bodyParam && `body: ${bodyParam}`, queryParams, multiQueryParams, headerParams + ' = {}'].filter(it => !!it);
   const resourcePath = path.replace(/{/g, '${params.');
   const methodName = methodDefinition.operationId ?? `${method}${pathName(path)}`;
   return `    async ${methodName}(${params.join(', ')}): Promise<(${returnType(oas, methodDefinition, imports)}) & {headers: Record<string, string>}>{
       const resource = '${path}';
       const path = \`${resourcePath}\`;
-      const result = await this.caller.call('${method.toUpperCase()}', resource, path, ${bodyParam ? 'JSON.stringify(body)': 'undefined'}, ${pathParams ? 'params' : '{}'}, queryParameters, headers);
+      const result = await this.caller.call('${method.toUpperCase()}', resource, path, 
+      ${bodyParam ? 'JSON.stringify(body)' : 'undefined'}, ${pathParams ? 'params' : '{}'}, ${queryParams ? 'queryParameters' : '{}'}, ${multiQueryParams ? 'multiQueryParameters' : '{}'}, headers);
       ${bodyValue(oas, methodDefinition)}
       throw new Error(\`Unknown status \${result.statusCode} returned from \${path}\`)
     }`;
@@ -141,9 +154,9 @@ export function generateSdkFrom(oas: OAS): string {
     const name = oas.info.title.replace(/ /g, '') + 'Sdk';
     const paths = pathsFrom(oas);
     const imports = new Array<string>();
-    const sdkMethods = paths.flatMap(({path, definition}) => {
+    const sdkMethods = paths.flatMap(({ path, definition }) => {
       if (definition) {
-        return methodOperations(definition).map(({method, definition: operation}) => {
+        return methodOperations(definition).map(({ method, definition: operation }) => {
           const parameters = operationParameters(oas, operation);
           const pathParameters = parameters.filter(it => it.in === 'path').map(it => it.name);
           const queryParameters = parameters.filter(it => it.in === 'query').map(it => it.name);
@@ -162,6 +175,7 @@ export type Caller = {
       body: string | undefined,
       pathParameters: Record<string, string>,
       queryParameters: Record<string, string>,
+      multiQueryParameters: Record<string, string[]>,
       headers: Record<string, string>
     ): Promise<{ statusCode: number; body: string; headers: Record<string, string> }>;
   }
@@ -171,8 +185,8 @@ export class ${name} {
     private readonly caller: Caller
   ){}
 ${sdkMethods.join('\n\n')}
-}`
-  } catch(e) {
+}`;
+  } catch (e) {
     console.error(e);
     throw e;
   }
