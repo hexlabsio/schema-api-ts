d3dd96a8d89d7886c74f7dff09bd0a4d8da031e1
Chris Barbour <chris.barbour03@gmail.com>
2021-08-02 11:40:56 +0100

    pulling vars from server lookup

--- src/sdk-mapper.ts
+++ src/sdk-mapper.ts
@@ -196,13 +196,13 @@ export type Caller = {
 
 export class ${name} {
   constructor(
-    private readonly caller: Caller,${servers ? `\n    private readonly serverLookup: {${serverLookupTyped.join('; ')}},\n    public readonly servers: Array<{url: string; variables: Record<string, string>}> = [${servers.join(', ')}]` : ''}
+    private readonly caller: Caller,${servers ? `\n    private readonly serverLookup?: {${serverLookupTyped.join('; ')}},\n    public readonly servers: Array<{url: string; variables: Record<string, string>}> = [${servers.join(', ')}]` : ''}
   ){}
   
     private server(): string | undefined {
       if(this.servers.length === 0) return undefined;
       const server = !this.serverLookup ? this.servers[0] : this.servers.find(it => Object.keys(this.serverLookup).reduce((result, key) => result && (!(this.serverLookup as any)[key] || it.variables[key] === (this.serverLookup as any)[key]), true as boolean));
-      return server && Object.keys(server).filter(it => it !== 'url').reduce((url, key) => url.replace(\`{\${key}}\`, server.variables[key]), server.url);
+      return server && Object.keys(server.variables).reduce((url, key) => url.replace(\`{\${key}}\`, server.variables[key]), server.url);
     }
   
 ${sdkMethods.join('\n\n')}
--- test/schema-example.ts
+++ test/schema-example.ts
@@ -1,114 +1,89 @@
-import {OASServer, SchemaBuilder, OpenApiSpecificationBuilder, OASSecurity} from '../src';
+import {OASServer} from "../dist";
+import {OpenApiSpecificationBuilder, SchemaBuilder} from "../src";
+
+const builder = SchemaBuilder.create();
 
 const servers: OASServer[] = [
   {
-    url: 'https://api.{environment}.klouds.io/{basePath}',
+    url: 'https://api.{environment}.xyz.io/{basePath}',
     variables: { environment: { default: 'dev' },  basePath: { default: 'views' } }
   },
   {
-    url: 'https://api.klouds.io/{basePath}',
+    url: 'https://api.xyz.io/{basePath}',
     variables: { environment: { default: 'prod' },  basePath: { default: 'views' } }
   },
   {
-    url: 'http://localhost:{port}', variables: {  environment: { default: 'local' }, port: { default: '3001' } }
+    url: 'http://localhost:{port}', variables: {  environment: { default: 'local' }, port: { default: '3000' } }
   },
 ];
-const builder = SchemaBuilder.create();
-const role = builder.anyOf(builder.stringWith({const: 'read'} as const), builder.stringWith({const: 'write'} as const), builder.stringWith({const: 'admin'} as const));
 
-export const schemasComponent = builder
-.add('HydraOperation', s => s.hydraOperation())
-.add('View', s => s.object({viewId: s.string(), viewName: s.string(), awsAccountId: s.string()}, undefined, false))
-.add('ViewResource', s => s.hydraResource('View'))
-.add('ViewCollection', s => s.hydraCollection('View'))
-.add('CreateView', s => s.object({name: s.string(), credentials: s.string()}))
-.add('PatchView', s => s.object(undefined, {name: s.string(), credentials: s.string()}))
-.add('AccessGrant', s => s.object({name: s.string(), role }))
-.add('UpdateAccess', s => s.object(undefined, {users: s.array(s.reference('AccessGrant')), groups:s.array(s.reference('AccessGrant'))}))
-.add('SecretSuccess', s => s.object({credentials: s.string(), awsAccountId: s.string()}))
-.add('ViewAccess', s => s.object({viewId: s.string(), role }))
-.add('ViewAccessSuccess', s => s.array(s.reference('ViewAccess')))
-.build();
-
-const securitySchemes = { OAuth: { type: 'oauth', flows: { authorizationCode: { authorizationUrl: '', scopes: { read: 'Read', write: 'Write', admin: 'Admin' }}} }};
-
-const viewIdParameter = { in: "path", required: true, name: 'viewId', schema: builder.string() } as const;
-const read: OASSecurity[] = [{OAuth: ['read', 'write', 'admin']}];
-const write: OASSecurity[] = [{OAuth: ['write', 'admin']}];
-const admin: OASSecurity[] = [{OAuth: ['admin']}];
-
-export const oasBuilder = OpenApiSpecificationBuilder.create(schemasComponent, { title: 'View Service Public Api', version: '1.0' })
-.add('servers', () => servers)
-.addComponent('securitySchemes', () => securitySchemes)
-.addComponent('responses', o => ({
-  'BadRequest': {description: 'Bad Request', content: o.textContent('Bad Request')}
+const schemas = builder.add('Chicken', s => s.object({
+  identifier: s.string(),
+  type: s.string(),
+  name: s.string()
 }))
+.add('ChickenCollection', s => s.array(s.reference('Chicken')))
+.add('Schema', s => s.object(undefined, undefined, true))
+.add('ChickenCreateRequest', s => s.object({
+  type: s.string(),
+  name: s.string()
+})).build();
+
+export default OpenApiSpecificationBuilder
+.create(schemas, { title: 'Chicken Store API', version: '1.0.0'})
+.add('servers', o => servers)
 .add('paths', o => ({
-  '/view': {
+  '/chicken': {
     get: {
-      operationId: 'getViews',
-      security: read,
+      operationId: 'getChickens',
       responses: {
-        200: {description: '', content: o.jsonContent('ViewCollection')},
-        400: o.responseReference('BadRequest')
+        200: {description: 'The Flock', content: o.jsonContent('ChickenCollection')},
       }
     },
     post: {
-      operationId: 'createView',
-      security: write,
-      parameters: [
-        { in: "header", required: true, name: 'X-Encryption-Key', schema: builder.string()}
-      ],
-      requestBody: { content: o.jsonContent('CreateView') },
+      operationId: 'createChicken',
+      requestBody: {
+        description: 'A Chicken',
+        content: o.jsonContent('ChickenCreateRequest')
+      },
       responses: {
-        201: {description: '', content: o.jsonContent('ViewResource')}
+        201: {
+          description: 'The Flock',
+          content: o.jsonContent('ChickenCollection')
+        },
+        400: {description: 'Bad Request', content: o.textContent()}
       }
     }
   },
-  '/view/{viewId}': {
+  '/chicken/{chickenId}': {
     get: {
-      operationId: 'getView',
-      security: read,
-      parameters: [viewIdParameter],
+      operationId: 'getChicken',
+      parameters: [{required: true, name: 'chickenId', in: 'path'}],
       responses: {
-        200: {description: '', content: o.jsonContent('ViewResource')}
+        200: {description: 'The Chicken', content: o.jsonContent('Chicken')},
       }
     },
-    patch: {
-      operationId: 'updateView',
-      security: write,
-      parameters: [viewIdParameter],
-      requestBody: { content: o.jsonContent('PatchView') },
+    put: {
+      operationId: 'updateChicken',
+      parameters: [{required: true, name: 'chickenId', in: 'path'}],
+      requestBody: {
+        description: 'A Chicken',
+        content: o.jsonContent('ChickenCreateRequest')
+      },
       responses: {
-        200: {description: '', content: o.jsonContent('ViewResource')}
+        200: {description: 'The Chicken', content: o.jsonContent('Chicken')},
       }
     },
     delete: {
-      operationId: 'deleteView',
-      security: admin,
-      parameters: [viewIdParameter],
+      operationId: 'deleteChicken',
+      parameters: [{required: true, name: 'chickenId', in: 'path'}],
       responses: {
-        200: {description: '', content: o.textContent('Deleted')}
+        200: {description: 'Success', content: o.textContent('Deleted')},
       }
     },
   },
-  '/view/{viewId}/access': {
-    post: {
-      operationId: 'updateAccess',
-      security: write,
-      parameters: [viewIdParameter],
-      responses: {
-        200: {description: '', content: o.jsonContent('UpdateAccess')},
-        403: {description: '', content: o.textContent('Access Denied')}
-      }
-    }
-  },
   '/schema': {
-    get: {
-      security: read,
-      responses: {
-        200: {description: '', content: { 'application/json': { schema: builder.object()}}}}
-    }
-  },
+    get: { responses: { 200: {description:'Schema', content: o.jsonContent('Schema') } } }
+  }
 }))
-export default oasBuilder.build();
+.build();
