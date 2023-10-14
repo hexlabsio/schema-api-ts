687c1f4261af1b88b1e8076690d8c9ada4bcc892
Chris Barbour <chris.barbour@hexlabs.io>
2023-10-14 23:18:57 +0100

    updating rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 
 export default {
-    input: 'src/index.ts',
+    input: ['src/index.ts', 'src/cli.ts'],
     output: {
         dir: 'dist',
         format: 'esm',
         entryFileNames: '[name].mjs'
     },
     plugins: [
-        typescript()
+        json({ compact: true }),
+        typescript(),
+        commonjs(),
+        resolve({ preferBuiltins: true, exportConditions: ['node'] }),
     ]
 };
