2f2f570dea5a0715663de20263bc0d2d431ce7f6
Chris Barbour <chris.barbour@hexlabs.io>
2023-10-14 22:37:23 +0100

    outputting mjs

---
+++ rollup.config.ts
@@ -0,0 +1,13 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        dir: 'dist',
+        format: 'esm',
+        entryFileNames: '[name].mjs'
+    },
+    plugins: [
+        typescript()
+    ]
+};
