8a128fd16ad5355124a76a30a67871e3d9567255
Chris Barbour <chris.barbour@hexlabs.io>
2025-09-28 11:49:22 +0100

    attempting to replace bad refs

--- src/schema-builder.ts
+++ src/schema-builder.ts
@@ -10,7 +10,9 @@ export class ZodSchemaBuilder<R extends Record<string, JsonSchema7Type>> {
   }
 
   add<S extends string, T extends z.ZodType>(name: S, type: T): ZodSchemaBuilder<R & {[name in S]: JsonSchema7Type}> {
-    this.schemas[name] = (zodToJsonSchema(type, {name, basePath: ['#','components', 'schemas'], '$refStrategy': 'none' }).definitions as any)[name];
+    this.schemas[name] = (zodToJsonSchema(type, {name, basePath: ['#','components', 'schemas'], '$refStrategy': 'root', postProcess: s => {
+      return JSON.parse(JSON.stringify(s).replaceAll('#/components/schemas/definitions/', '#/components/schemas/'))
+      } }).definitions as any)[name];
     this.schemas[name].title = name;
     return this as any;
   }
